b9f4eb17d1dd1256638a41d50fddfe1ea662edcf
Keskin, Ogulcan <Ogulcan.Keskin@adesso.com.tr>
2022-02-07 12:43:34 +0300

    Initial Commit

---
+++ UIKitLibrary/UIKitLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIKitLibraryApp.swift
+//  UIKitLibrary
+//
+//  Created by Ogulcan Keskin on 7.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct UIKitLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
